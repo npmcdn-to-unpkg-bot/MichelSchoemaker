49feeab9cf288af2f12958d0623bd5f4600f032f
michaelsch92 <michel.schoemaker@gmail.com>
2016-06-18 11:23:53 -0700

    added scrolling effects

--- js/main.js
+++ js/main.js
@@ -6,3 +6,49 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
 var DEBUG = false;
 // Accepts any class name
 var rellax = new Rellax('.rellax');
+var h = $(window).height()
+
+$(function() {
+    // Init Controller
+    var controller = new ScrollMagic.Controller();
+    //create animation
+    var aboutTween = new TimelineMax()
+    .to('#about', 2, {height:80, opacity:1})
+    //create a scene
+    new ScrollMagic.Scene({
+      duration:200,
+      offset:h/4,
+    })
+    .setTween(aboutTween)
+    .addTo(controller);
+
+    var softwareTween = new TimelineMax()
+    .to('#about', 2, {height:70, opacity:0.7})
+    .to('#software', 2, {height:80, opacity:1})
+    new ScrollMagic.Scene({
+      duration:200,
+      offset:h/4+h,
+    })
+    .setTween(softwareTween)
+    .addTo(controller);
+
+    var hardwareTween = new TimelineMax()
+    .to('#software', 2, {height:70, opacity:0.7})
+    .to('#hardware', 2, {height:80, opacity:1})
+    new ScrollMagic.Scene({
+      duration:200,
+      offset:h/4+2*h,
+    })
+    .setTween(hardwareTween)
+    .addTo(controller);
+
+    var contactTween = new TimelineMax()
+    .to('#hardware', 2, {height:70, opacity:0.7})
+    .to('#contact', 2, {height:80, opacity:1})
+    new ScrollMagic.Scene({
+      duration:200,
+      offset:h/4+3*h,
+    })
+    .setTween(contactTween)
+    .addTo(controller);
+});
